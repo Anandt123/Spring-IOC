89ae80420d5a43102fc655a9fa062ec1292df45d
Anandkumar <74614443+Anandt123@users.noreply.github.com>
2021-03-17 15:50:40 +0530

    Add files via upload

---
+++ ApplicationContext/src/Type3/Longdrive.java
@@ -0,0 +1,21 @@
+package com.jspiders.pkg3;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class Longdrive {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+		//BeanFactory container = new XmlBeanFactory(new FileSystemResource("config.xml"));
+		WithGF g1 = (WithGF)container.getBean("anand12");
+		g1.ondrive();
+		WithoutGF g2 = (WithoutGF)container.getBean("anand21");
+		g2.Ondrive();
+	}
+
+}
